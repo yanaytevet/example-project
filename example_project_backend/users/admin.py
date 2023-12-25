1837ebcb1cd5f59c34b3f4514023814c8503244b
Yanay Tevet <yanay@xperiti.com>
2023-12-25 13:46:15 +0200

    add organization

--- example_project_backend/users/admin.py
+++ example_project_backend/users/admin.py
@@ -28,7 +28,6 @@ class CustomUserAdmin(UserAdmin):
             {
                 'fields': (
                     'permissions',
-                    'organization',
                 ),
             },
         ),
